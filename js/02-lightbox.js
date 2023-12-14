7d0b20e7b322bb321b7f8bddfaf94e1416c05ceb
Maciej Derda <maciejderda@wp.pl>
2023-12-14 23:55:46 +0100

    update
    update 01-/02- .html and .js files

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryContainer = document.querySelector(".gallery");
+const images = galleryItems.map(({ preview, original, description }) => {
+  return `<li>
+        <a class="gallery__item" href="${original}">
+            <img 
+                class="gallery__image" 
+                src="${preview}" 
+                alt="${description}" 
+            />
+        </a>
+    </li>`;
+});
+galleryContainer.insertAdjacentHTML("beforeend", images.join(""));
 
-console.log(galleryItems);
+const galleryImages = document.querySelectorAll(".gallery__item");
+
+new SimpleLightbox(galleryImages, {
+  captionsData: "alt",
+  captionDelay: 250,
+  captionPosition: "bottom",
+});
